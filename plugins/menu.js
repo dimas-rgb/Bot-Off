12192c8c7a44ac953f19970d165c19afcf26b559
dimas-rgb <78903714+dimas-rgb@users.noreply.github.com>
2021-02-12 07:17:08 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -88,8 +88,8 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 ╭════•›「 Sosmed 」
 ╿ Github :
 ╿ https://github.com/Arya274/Arya-BOT3
-╿ Youtube : Drawl Nag
-╿ Instagram : @arpunchs
+╿ Youtube : ---
+╿ Instagram : ---
 ╰═══════════════
 
 ╭════•›「 Rules 」
